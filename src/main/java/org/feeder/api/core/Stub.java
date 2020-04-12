4040fee27b04552aa7702d076c98c711293cf2c9
Andrei Varashen <voroshen.av@gmail.com>
2020-04-12 15:22:06 +0300

    feature: add work flows and remote package repo (#1)

---
+++ src/main/java/org/feeder/api/core/Stub.java
@@ -0,0 +1,14 @@
+package org.feeder.api.core;
+
+public class Stub {
+
+  private String field;
+
+  public String getField() {
+    return field;
+  }
+
+  public void setField(String field) {
+    this.field = field;
+  }
+}
