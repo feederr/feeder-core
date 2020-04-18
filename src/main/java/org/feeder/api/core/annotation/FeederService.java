e172e8ae40a2629de4200f97fb856bfd49cebef0
avarashen <avarashen@exadel.com>
2020-04-18 13:23:17 +0300

    add jpa auditing support

--- src/main/java/org/feeder/api/core/annotation/FeederService.java
+++ src/main/java/org/feeder/api/core/annotation/FeederService.java
@@ -6,6 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.feeder.api.core.configuration.AsyncApplicationEventConfiguration;
 import org.feeder.api.core.configuration.ExceptionHandlingConfiguration;
+import org.feeder.api.core.configuration.JpaAuditingConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
@@ -13,7 +14,8 @@ import org.springframework.context.annotation.Import;
 @Retention(RetentionPolicy.RUNTIME)
 @Import({
     ExceptionHandlingConfiguration.class,
-    AsyncApplicationEventConfiguration.class
+    AsyncApplicationEventConfiguration.class,
+    JpaAuditingConfiguration.class
 })
 @SpringBootApplication
 public @interface FeederService {
--- src/main/java/org/feeder/api/core/domain/BaseEntity.java
+++ src/main/java/org/feeder/api/core/domain/BaseEntity.java
@@ -1,11 +1,14 @@
 package org.feeder.api.core.domain;
 
+import javax.persistence.EntityListeners;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Setter
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity<ID> implements Persistable<ID> {
 
   private transient boolean isNew = false;
