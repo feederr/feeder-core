5f94ca6d5eebe30cb0eb817cc72f985c8fe6b08e
avarashen <avarashen@exadel.com>
2020-06-03 03:27:20 +0300

    add custom serializer/deserializer

--- src/main/java/org/feeder/api/core/configuration/KafkaConfiguration.java
+++ src/main/java/org/feeder/api/core/configuration/KafkaConfiguration.java
@@ -1,5 +1,11 @@
 package org.feeder.api.core.configuration;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
@@ -7,6 +13,7 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
@@ -14,6 +21,8 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @EnableKafka
 @Configuration
@@ -31,7 +40,13 @@ public class KafkaConfiguration {
 
   @Bean
   public ProducerFactory<?, ?> producerFactory() {
-    return new DefaultKafkaProducerFactory<>(kafkaProperties.buildProducerProperties());
+    JsonSerializer<Object> jsonSerializer = new JsonSerializer<>(objectMapper());
+    jsonSerializer.setAddTypeInfo(false);
+    return new DefaultKafkaProducerFactory<>(
+        kafkaProperties.buildProducerProperties(),
+        null,
+        jsonSerializer
+    );
   }
 
   @Bean
@@ -48,7 +63,17 @@ public class KafkaConfiguration {
     ConcurrentKafkaListenerContainerFactory<Object, Object> factory =
         new ConcurrentKafkaListenerContainerFactory<>();
     configurer.configure(factory, consumerFactory);
-
+    factory.setMessageConverter( new StringJsonMessageConverter( objectMapper() ) );
     return factory;
+  }
+
+  private static ObjectMapper objectMapper() {
+    return new Jackson2ObjectMapperBuilder()
+        .modules(new JavaTimeModule(), new Jdk8Module())
+        .serializationInclusion(JsonInclude.Include.NON_EMPTY)
+        .featuresToEnable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
+        .featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+            SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .build();
   }
 }
